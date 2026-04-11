a5d09bf40306a375fcf641db2d95d47d30ef912e
jangwoo0305 <tjwkddn970305@gmail.com>
2026-04-11 17:55:16 +0900

    드래그 하는 방향으로 카메라 이동

--- Assets/Scripts/Camera Controller.cs
+++ Assets/Scripts/Camera Controller.cs
@@ -7,15 +7,20 @@ public class CameraController : MonoBehaviour
 {
     public GameObject _player;  
     public Vector3 _offset;
+    public float _moveSpeed = 10f;
+    public float _edgeSize = 20f;
 
     public void Start()
     {
         _offset = transform.position - _player.transform.position;
+        
+        
     }
     
     private void LateUpdate()
     {
         CameraMoveToPlayer();
+        CameraMoveByMouse();
     }
 
     public void CameraMoveToPlayer()
@@ -24,5 +29,24 @@ public class CameraController : MonoBehaviour
         {
             transform.position = _player.transform.position + _offset;
         }
+    }
+
+    public void CameraMoveByMouse()
+    {
+        Vector3 moveDir = Vector3.zero;
+        Vector3 mousePos = Input.mousePosition;
+
+        if (mousePos.x <= _edgeSize)
+            moveDir += -transform.right;
+        if (mousePos.x >= Screen.width - _edgeSize)
+            moveDir += transform.right;
+        if (mousePos.y <= _edgeSize)
+            moveDir += -transform.forward;
+        if (mousePos.y >= Screen.height - _edgeSize)
+            moveDir += transform.forward;
+
+        moveDir.y = 0; 
+        moveDir.Normalize();
+        transform.position += moveDir * _moveSpeed * Time.deltaTime;
     }
 }
